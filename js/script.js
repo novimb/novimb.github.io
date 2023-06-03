df67509102df36d287d757370689f5f1ec554c70
nobimb <monik.nik@mail.ru>
2023-06-03 15:52:29 +0300

    v1.2

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
+/input block/
 var firstname = "Михаил"
 var secondname = "Лахметкин"
-document.getElementById("Data").textContent = firstname+" "+secondname;   
-   
+document.getElementById("Data").textContent = firstname+" "+secondname;
 
-/input block/
-var keyBox = document.search.key;
-function onchange(e){
-    var printBlock = document.getElementById("printBlock");
-    firstname = e.target.value;
+document.getElementById("print").addEventListener("click", function() {
+    firstname = document.getElementById("key").value;
+    secondname = document.getElementById("key2").value;
     document.getElementById("Data").textContent = firstname+" "+secondname; 
-}
-keyBox.addEventListener("change", onchange);
-
-var keyBox2 = document.search.key2;
-function onchange2(e){
-    var printBlock = document.getElementById("printBlock");
-    secondname = e.target.value;
-    document.getElementById("Data").textContent = firstname+" "+secondname; 
-}
-keyBox2.addEventListener("change", onchange2);
+});
 
 /button for random change background/
+var BackGNum = 0;  
 window.onload = function() {
     var button = document.getElementById('input_button_bg_change');
     var body = document.getElementsByTagName('body')[0];
-    var colors = ['blue', 'grey', 'white', 'red', 'green', '#aaa', '#FFAACC', 'rgb(122,111,110)'];
+    var colors = ['white', 'grey', 'blue', 'red', 'green', '#aaa', '#FFAACC', 'rgb(122,111,110)'];
     button.onclick = function() {
-        body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+        var a = Math.floor(Math.random() * colors.length);
+        if(a == BackGNum){
+          BackGNum = a + 1;
+          if(BackGNum >= colors.length){
+            BackGNum = 0;
+          }
+        }
+        else{
+          BackGNum = a;
+        }
+        body.style.backgroundColor = colors[BackGNum];
     };
 };
 
